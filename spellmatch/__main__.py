c09c838a234dcc5a7e6ee889fee229a1ff76642c
Jonas Windhager <jonas.windhager@uzh.ch>
2022-02-13 12:58:03 +0100

    CLI

--- spellmatch/__main__.py
+++ spellmatch/__main__.py
@@ -1,6 +1,21 @@
-def main():
+import click
+
+
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+
+@cli.command()
+def register():
+    pass
+
+
+@cli.command()
+def match():
     pass
 
 
 if __name__ == "__main__":
-    main()
+    cli()
