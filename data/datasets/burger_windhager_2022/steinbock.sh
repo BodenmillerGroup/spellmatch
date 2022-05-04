d121cf1f926667a90ca9c242d297f18e8f8cc62a
Jonas Windhager <jonas.windhager@uzh.ch>
2022-05-04 13:05:03 +0200

    Burger et al

---
+++ data/datasets/burger_windhager_2022/steinbock.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+shopt -s expand_aliases
+
+alias steinbock="docker run -v /mnt/data/git/spellmatch/data/datasets/burger_windhager_2022:/data -u $(id -u):$(id -g) ghcr.io/bodenmillergroup/steinbock:0.13.5"
+
+# # panel.csv has been manually annotated!
+# steinbock preprocess imc panel
+
+# # images.csv has been manually annotated!
+# steinbock preprocess imc images --hpf 50
