769b7f8785f7aeace84caa5ffe4f7f8af5096f18
Jonas Windhager <jonas.windhager@uzh.ch>
2022-05-07 01:28:37 +0200

    Kuett & Catena run_spellmatch.sh

--- data/datasets/kuett_catena_2022/run_spellmatch.sh
+++ data/datasets/kuett_catena_2022/run_spellmatch.sh
@@ -5,10 +5,11 @@ spellmatch register intensities source_img target_img --source-channel "Histone 
 
 spellmatch match source_masks target_masks --algorithm icp --algorithm-args "max_dist=15,min_change=1.0e-9" --prior-transforms refined_transforms scores_icp_forward
 spellmatch match source_masks target_masks --algorithm icp --algorithm-args "max_dist=15,min_change=1.0e-9" --prior-transforms refined_transforms --reverse scores_icp_reverse
-spellmatch assign scores_icp_forward --reverse-scores scores_icp_reverse --max-only --direction union --validate initial_assignments --source-masks source_masks --target-masks target_masks --show 100 assignments_icp
+spellmatch assign scores_icp_forward --reverse-scores scores_icp_reverse --max --direction intersect --validate initial_assignments --source-masks source_masks --target-masks target_masks --show 100 assignments_icp
 
-# spellmatch match source_masks target_masks --algorithm spellmatch --algorithm-args "transform_tol=1.0e-3,intensity_transform=numpy.log1p,adj_radius=15.0,degree_weight=0.0,intensity_weight=1.0,celldist_weight=0.0,spatial_cdist_prior_thres=15.0,max_spatial_cdist=15.0" --source-images source_img --target-images target_img --prior-transforms refined_transforms scores_spellmatch_forward
-# spellmatch match source_masks target_masks --algorithm spellmatch --algorithm-args "transform_tol=1.0e-3,intensity_transform=numpy.log1p,adj_radius=15.0,degree_weight=0.0,intensity_weight=1.0,celldist_weight=0.0,spatial_cdist_prior_thres=15.0,max_spatial_cdist=15.0" --source-images source_img --target-images target_img --prior-transforms refined_transforms --reverse scores_spellmatch_reverse
-# spellmatch assign scores_spellmatch_forward --strategy forward_max --score-thres 0 --validate initial_assignments --source-masks source_masks --target-masks target_masks --show 100 assignments_spellmatch_forward
-# spellmatch assign scores_spellmatch_reverse --strategy reverse_max --score-thres 0 --validate assignments_spellmatch_forward --source-masks source_masks --target-masks target_masks --show 100 assignments_spellmatch_reverse
-# spellmatch combine assignments_spellmatch_forward assignments_spellmatch_reverse --strategy union --validate initial_assignments --source-masks source_masks --target-masks target_masks --show 100 assignments_spellmatch
+spellmatch match source_masks target_masks --algorithm rigid_cpd --algorithm-args "max_dist=15,update_scale=no" --prior-transforms refined_transforms scores_rigid_cpd_forward
+spellmatch match source_masks target_masks --algorithm rigid_cpd --algorithm-args "max_dist=15,update_scale=no" --prior-transforms refined_transforms --reverse scores_rigid_cpd_reverse
+spellmatch assign scores_rigid_cpd_forward --reverse-scores scores_rigid_cpd_reverse --max --direction intersect --validate initial_assignments --source-masks source_masks --target-masks target_masks --show 100 assignments_rigid_cpd
+
+spellmatch match source_masks target_masks --algorithm spellmatch --algorithm-args "intensity_transform=numpy.arcsinh,scores_tol=1.0e-9,spatial_cdist_prior_thres=15,max_spatial_cdist=15,degree_weight=1,intensity_weight=1,intensity_interp_lmd=1,distance_weight=1" --source-images source_img --target-images target_img --prior-transforms refined_transforms scores_spellmatch
+spellmatch assign scores_spellmatch --max --direction intersect --validate initial_assignments --source-masks source_masks --target-masks target_masks --show 100 assignments_spellmatch
